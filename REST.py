dc77a0961135e957148b45b6a9c3b98ded803b93
Aravind Venkata Punugu <tannishpage1@gmail.com>
2020-08-16 00:01:46 +1000

    Got the mobile app POSTing working

--- REST.py
+++ REST.py
@@ -18,7 +18,6 @@ m_app_data = ['date', 'time', 'type_of_plastic', 'Longitude', 'Latitude']
 
 LOGF = open("log.txt", 'w')
 app = Flask(__name__)
-IMG_ROOT = "./images"
 
 @app.route('/sensor', methods=('GET', 'POST'))
 def received_sensor():
@@ -33,11 +32,13 @@ def received_mobile_app():
     if request.method == 'POST':
         image = request.files['image']
         data = get_m_app_data(request)
-        image.save(IMG_ROOT)
-        #location = os.path.join(IMG_ROOT, image.name)
-        #add_app_data_into_database(location, data)
-        return ",".join(data) + "," + str(dir(image))
-        #add_app_data_into_database(image, data.decode().split(','))
+        #os.chdir("images")
+        #image.save(os.getcwd())
+        location = os.path.join(os.getcwd(), image.filename)
+        image.save(location)
+        return add_app_data_into_database(location, data)
+    else:
+        return time.time()
 
 
 def add_sensor_data_to_database(data:list):
@@ -54,19 +55,26 @@ def add_sensor_data_to_database(data:list):
         DBConn.close()
         return "201 Created"
     except Exception as e:
-        LOGF.write("[{}]\t".format(str(datetime.datetime.now())) + str(e) + "\n\n")
+        LOGF.write("[{}]\t".format(str(datetime.datetime.now())) + str(e.with_traceback()) + "\n\n")
         LOGF.flush()
         print(e)
         return "500 Internal Server Error"
 
-def add_app_data_into_database(image, data:list):
-    DBConn = sqlite3.connect(DBF)
-    c = DBConn.cursor()
-    location = save(image)
-    c.execute("INSERT INTO Data_From_App ('{}', '{}', '{}', '{}', '{}', '{}')".format(location, data[0], data[1], data[2], data[3], data[4]))
-    DBConn.commit()
-    DBConn.close()
-    return "201 Created"
+def add_app_data_into_database(location, data:list):
+    if (len(data) != 5):
+        return "510 Not Extended"
+    try:
+        DBConn = sqlite3.connect(DBF)
+        c = DBConn.cursor()
+        c.execute("INSERT INTO Data_From_App VALUES ('{}', '{}', '{}', '{}', '{}', '{}')".format(location, data[0], data[1], data[2], data[3], data[4]))
+        DBConn.commit()
+        DBConn.close()
+        return "201 Created"
+    except KeyboardInterrupt as e:
+        LOGF.write("[{}]\t".format(str(datetime.datetime.now())) + str(e.with_traceback()) + "\n\n")
+        LOGF.flush()
+        print(e)
+        return "500 Internal Server Error"
 
 def get_m_app_data(request):
     data = []
